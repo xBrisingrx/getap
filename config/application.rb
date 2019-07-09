0e71a27387b05d203e6efff2635bb9e5a3c36ded
Eragon <mdavid.almiron@gmail.com>
2019-07-09 19:31:39 -0300

    Configuracion de la base de datos
    Agrego en application.rb la configuracion para poder usar el archivo local_env. De esa forma no se comparten datos sensibles en el repositorio. En local_env cada uno debera ingresar los datos correspondientes a su BD. En este mismo archivo mas adelante se pondra informacion como la llave secreta y usuario para poder enviar mails.

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,13 @@ module Getap
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    
+    # Configuracion para usar variables de entorno
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
   end
 end
